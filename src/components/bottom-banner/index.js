1cd5f9189132dda617a9608e8eae6783b4213702
DESKTOP-Q8RBGJI\Violeta <violetforest5@protonmail.com>
2021-11-19 19:34:25 -0500

    about page

--- src/components/bottom-banner/index.js
+++ src/components/bottom-banner/index.js
@@ -3,6 +3,6 @@ import styles from './styles.module.scss'
 
 export const BottomBanner = () => {
   return <div className={styles.container}>
-    This is a forked version of the opensource project <a href="https://github.com/hicetnunc2000/hicetnunc" target="_blank">Hicetnunc</a> with additional features. The code is opensource and available <a href="https://github.com/violetforest/hicetnunc" target="_blank">here</a>.
+    This is a forked version of the opensource project <a href="https://github.com/hicetnunc2000/hicetnunc" target="_blank">Hicetnunc</a> with additional features. The code is opensource and available <a href="https://github.com/violetforest/hicetnunc" target="_blank">here</a>. Check the about page for the list of features.
   </div>
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Page, Container, Padding } from '../../components/layout'
-import { Button, Primary } from '../../components/button'
+import { Primary, Button } from '../../components/button'
 import { HicetnuncContext } from '../../context/HicetnuncContext'
 import { BottomBanner } from '../../components/bottom-banner'
 import { getLanguage } from '../../constants'
@@ -27,40 +27,33 @@ export class About extends Component {
         
         <Container>
           <Padding>
-            <div className={styles.buttons}>
+            <div>
+              This site includes features that were developed during HEN but never pushed to production, and possibly new features in the future.
+            </div>
+            &nbsp;
+            <div>
               <strong>Additional features:</strong>
             </div>
+            &nbsp;
+            <ul>
+              <li>
+                rollover on OBJKTs in profile pages shows title, price, and collect button (designed and coded by @crzypatchwork (rafael lima), @_ufffd (kyle grover), & @violet_forest)
+              </li>
               &nbsp;
-                <p>
-                  + rollover on OBJKTs in profile pages shows title, price, and collect button (designed and coded by @crzypatchwork (rafael lima), UFFD (kyle grover), and violetforest)
-                </p>
-                <p>
-                  + feed of latest mints by wallets of which you've collected (designed and coded by @violet_forest and @secondcass)
-                </p>
+              <li>
+              feed of latest mints by wallets of which you've collected (designed and coded by @violet_forest and @secondcass (Cass) (Coming soon)
+              </li>
+              &nbsp;
+              <li>
+              finish language selection feature started by @andrevenancio (Coming soon, maybe)
+              </li>
+            </ul>
           </Padding>
         </Container>
 
         <Container>
           <Padding>
-            <strong>hic et nunc</strong>
-          </Padding>
-        </Container>
-
-        <Container>
-          <Padding>
-            <p>{this.language.about.paragraphs[0]}</p>
-          </Padding>
-        </Container>
-
-        <Container>
-          <Padding>
-            <p>{this.language.about.paragraphs[1]}</p>
-          </Padding>
-        </Container>
-
-        <Container>
-          <Padding>
-            <p>{this.language.about.paragraphs[2]}</p>
+            <strong>This site is not in charge of fees.</strong>
           </Padding>
         </Container>
 
